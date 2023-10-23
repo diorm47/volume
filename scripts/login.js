d79ccaba46d54db59d2c680f5b30ec253fd60662
diorm47 <dior47m@gmail.com>
2023-10-23 23:48:41 +0500

    dfvsdf

--- scripts/login.js
+++ scripts/login.js
@@ -4,18 +4,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const clearUsername = document.getElementById("clearUsername");
   const togglePassword = document.getElementById("togglePassword");
   const error = document.getElementById("error");
+  const error_email = document.querySelector(".error_email");
   const submitBtn = document.getElementById("submitBtn");
   const clearUsernameIcon = document.getElementById("clearUsername");
   const togglePasswordIcon = document.getElementById("togglePassword");
-
+ console.log(error_email);
+  const validateEmail = (email) => {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(String(email).toLowerCase());
+  };
   clearUsername.addEventListener("click", () => {
     username.value = "";
   });
   const checkButtonState = () => {
     submitBtn.disabled = !(username.value && password.value.length >= 8);
   };
-  username.addEventListener("input", () => {
-    clearUsernameIcon.style.display = username.value ? "block" : "none";
+  username.addEventListener("blur", () => {
+    if (!validateEmail(username.value)) {
+      error_email.textContent = "Непрвильный email";
+      submitBtn.disabled = true;
+      username.classList.add("error_input");
+    } else {
+      error_email.textContent = "";
+      submitBtn.disabled = false;
+      username.classList.remove("error_input");
+    }
     checkButtonState();
   });
   password.addEventListener("input", () => {
